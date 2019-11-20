90dd9b3ec145c049cebae31a825c863933ea5655
Nikita Volkov <nikita.y.volkov@mail.ru>
2019-11-20 21:01:56 +0300

    Rename Tail to Body

--- library/HeadedMegaparsec.hs
+++ library/HeadedMegaparsec.hs
@@ -55,7 +55,7 @@ Provides for composition between consecutive megaparsec `try` blocks.
 -}
 data HeadedParsec err strm a =
   HeadHeadedParsec (Parsec err strm (Either a (Parsec err strm a))) |
-  TailHeadedParsec (Parsec err strm a)
+  BodyHeadedParsec (Parsec err strm a)
 
 
 -- * Instances
@@ -67,7 +67,7 @@ data HeadedParsec err strm a =
 instance Functor (HeadedParsec err strm) where
   fmap fn = \ case
     HeadHeadedParsec p -> HeadHeadedParsec (fmap (bimap fn (fmap fn)) p)
-    TailHeadedParsec p -> TailHeadedParsec (fmap fn p)
+    BodyHeadedParsec p -> BodyHeadedParsec (fmap fn p)
 
 instance (Ord err, Stream strm) => Applicative (HeadedParsec err strm) where
   pure = HeadHeadedParsec . pure . pure . pure
@@ -85,38 +85,38 @@ instance (Ord err, Stream strm) => Selective (HeadedParsec err strm) where
               junction2 <- p2
               case junction2 of
                 Left aToB -> return (Left (aToB a))
-                Right tailP2 -> return (Right (fmap ($ a) tailP2))
-          Right tailP1 -> return $ Right $ do
-            eitherAOrB <- tailP1
+                Right bodyP2 -> return (Right (fmap ($ a) bodyP2))
+          Right bodyP1 -> return $ Right $ do
+            eitherAOrB <- bodyP1
             case eitherAOrB of
               Right b -> return b
               Left a -> do
                 junction2 <- try p2
                 case junction2 of
                   Left aToB -> return (aToB a)
-                  Right tailP2 -> fmap ($ a) tailP2
-      TailHeadedParsec p2 -> HeadHeadedParsec $ do
+                  Right bodyP2 -> fmap ($ a) bodyP2
+      BodyHeadedParsec p2 -> HeadHeadedParsec $ do
         junction1 <- p1
         case junction1 of
           Left eitherAOrB -> case eitherAOrB of
             Left a -> return (Right (fmap ($ a) p2))
             Right b -> return (Left b)
-          Right tailP1 -> return $ Right $ do
-            eitherAOrB <- tailP1
+          Right bodyP1 -> return $ Right $ do
+            eitherAOrB <- bodyP1
             case eitherAOrB of
               Left a -> fmap ($ a) p2
               Right b -> return b
-    TailHeadedParsec p1 -> \ hp2 -> TailHeadedParsec (selectA p1 (toParsec hp2))
+    BodyHeadedParsec p1 -> \ hp2 -> BodyHeadedParsec (selectA p1 (toParsec hp2))
 
 instance (Ord err, Stream strm) => Alternative (HeadedParsec err strm) where
   empty = HeadHeadedParsec empty
   (<|>) = \ case
     HeadHeadedParsec p1 -> \ case
       HeadHeadedParsec p2 -> HeadHeadedParsec (try p1 <|> p2)
-      TailHeadedParsec p2 -> TailHeadedParsec (try (toParsec (HeadHeadedParsec p1)) <|> p2)
-    TailHeadedParsec p1 -> \ case
-      HeadHeadedParsec p2 -> TailHeadedParsec (try p1 <|> toParsec (HeadHeadedParsec p2))
-      TailHeadedParsec p2 -> TailHeadedParsec (try p1 <|> p2)
+      BodyHeadedParsec p2 -> BodyHeadedParsec (try (toParsec (HeadHeadedParsec p1)) <|> p2)
+    BodyHeadedParsec p1 -> \ case
+      HeadHeadedParsec p2 -> BodyHeadedParsec (try p1 <|> toParsec (HeadHeadedParsec p2))
+      BodyHeadedParsec p2 -> BodyHeadedParsec (try p1 <|> p2)
 
 
 -- * Execution
@@ -131,8 +131,8 @@ toParsec = \ case
     junction <- try p
     case junction of
       Left a -> return a
-      Right tailP -> tailP
-  TailHeadedParsec p -> p
+      Right bodyP -> bodyP
+  BodyHeadedParsec p -> p
 
 
 -- *
@@ -154,4 +154,4 @@ Composing consecutive bodies results in one body.
 Composing consecutive head and body leaves the head still composable with preceding head.
 -}
 body :: (Stream strm) => Parsec err strm a -> HeadedParsec err strm a
-body = TailHeadedParsec
+body = BodyHeadedParsec
